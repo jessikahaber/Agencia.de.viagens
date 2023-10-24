01518dca9bc7c726ca815d44a8aa3233b2be22e3
JessikaHaber <jessikahaber@icloud.com>
2023-10-24 15:21:23 -0300

    att do bd

--- BancodeDados/BD Fisico_AgenciaTurismo.sql
+++ BancodeDados/BD Fisico_AgenciaTurismo.sql
@@ -1,6 +1,7 @@
 USE [master]
 GO
-/****** Object:  Database [BDAgenciaTurismo]    Script Date: 16/10/2023 01:20:46 ******/
+
+/****** Object:  Database [BDAgenciaTurismo]    Script Date: 24/10/2023 15:13:34 ******/
 CREATE DATABASE [BDAgenciaTurismo]
  CONTAINMENT = NONE
  ON  PRIMARY 
@@ -9,142 +10,108 @@ CREATE DATABASE [BDAgenciaTurismo]
 ( NAME = N'bdTurismo_log', FILENAME = N'C:\Program Files\Microsoft SQL Server\MSSQL16.MSSQLSERVERDEV2023\MSSQL\DATA\bdTurismo_log.ldf' , SIZE = 8192KB , MAXSIZE = 2048GB , FILEGROWTH = 65536KB )
  WITH CATALOG_COLLATION = DATABASE_DEFAULT, LEDGER = OFF
 GO
-ALTER DATABASE [BDAgenciaTurismo] SET COMPATIBILITY_LEVEL = 160
-GO
+
 IF (1 = FULLTEXTSERVICEPROPERTY('IsFullTextInstalled'))
 begin
 EXEC [BDAgenciaTurismo].[dbo].[sp_fulltext_database] @action = 'enable'
 end
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ANSI_NULL_DEFAULT OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ANSI_NULLS OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ANSI_PADDING OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ANSI_WARNINGS OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ARITHABORT OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET AUTO_CLOSE OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET AUTO_SHRINK OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET AUTO_UPDATE_STATISTICS ON 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET CURSOR_CLOSE_ON_COMMIT OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET CURSOR_DEFAULT  GLOBAL 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET CONCAT_NULL_YIELDS_NULL OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET NUMERIC_ROUNDABORT OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET QUOTED_IDENTIFIER OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET RECURSIVE_TRIGGERS OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET  DISABLE_BROKER 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET AUTO_UPDATE_STATISTICS_ASYNC OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET DATE_CORRELATION_OPTIMIZATION OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET TRUSTWORTHY OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ALLOW_SNAPSHOT_ISOLATION OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET PARAMETERIZATION SIMPLE 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET READ_COMMITTED_SNAPSHOT OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET HONOR_BROKER_PRIORITY OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET RECOVERY FULL 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET  MULTI_USER 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET PAGE_VERIFY CHECKSUM  
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET DB_CHAINING OFF 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET FILESTREAM( NON_TRANSACTED_ACCESS = OFF ) 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET TARGET_RECOVERY_TIME = 60 SECONDS 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET DELAYED_DURABILITY = DISABLED 
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET ACCELERATED_DATABASE_RECOVERY = OFF  
 GO
-EXEC sys.sp_db_vardecimal_storage_format N'BDAgenciaTurismo', N'ON'
-GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET QUERY_STORE = ON
 GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET QUERY_STORE (OPERATION_MODE = READ_WRITE, CLEANUP_POLICY = (STALE_QUERY_THRESHOLD_DAYS = 30), DATA_FLUSH_INTERVAL_SECONDS = 900, INTERVAL_LENGTH_MINUTES = 60, MAX_STORAGE_SIZE_MB = 1000, QUERY_CAPTURE_MODE = AUTO, SIZE_BASED_CLEANUP_MODE = AUTO, MAX_PLANS_PER_QUERY = 200, WAIT_STATS_CAPTURE_MODE = ON)
 GO
-USE [BDAgenciaTurismo]
-GO
-/****** Object:  Schema [Cadastro]    Script Date: 16/10/2023 01:20:46 ******/
-CREATE SCHEMA [Cadastro]
-GO
-/****** Object:  Table [Cadastro].[cliente]    Script Date: 16/10/2023 01:20:46 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Cadastro].[cliente](
-	[IdCliente] [int] IDENTITY(1,1) NOT NULL,
-	[CPF] [char](11) NOT NULL,
-	[nomeCompleto] [varchar](350) NOT NULL,
-	[logradouro] [varchar](250) NOT NULL,
-	[numero] [smallint] NOT NULL,
-	[complemento] [varchar](250) NULL,
-	[cep] [char](10) NOT NULL,
-	[bairro] [varchar](50) NOT NULL,
-	[cidade] [varchar](50) NOT NULL,
-	[estado] [varchar](50) NOT NULL,
-	[pais] [varchar](50) NOT NULL,
-	[telefone] [varchar](20) NOT NULL,
-	[email] [nvarchar](100) NOT NULL,
-	[senha] [varbinary](12) NOT NULL,
-	[DataCadastro] [datetime] NOT NULL,
- CONSTRAINT [PK_cliente] PRIMARY KEY CLUSTERED 
-(
-	[IdCliente] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-/****** Object:  Table [Cadastro].[Viagem]    Script Date: 16/10/2023 01:20:46 ******/
-SET ANSI_NULLS ON
-GO
-SET QUOTED_IDENTIFIER ON
-GO
-CREATE TABLE [Cadastro].[Viagem](
-	[id] [int] IDENTITY(1,1) NOT NULL,
-	[IdCliente] [int] NULL,
-	[LocalPartida] [varchar](60) NOT NULL,
-	[LocalDestino] [varchar](60) NOT NULL,
-	[DataPartida] [date] NOT NULL,
-	[DataRetorno] [date] NOT NULL,
-	[ValorDestino] [float] NOT NULL,
- CONSTRAINT [PK_tb_Viagemp] PRIMARY KEY CLUSTERED 
-(
-	[id] ASC
-)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON, OPTIMIZE_FOR_SEQUENTIAL_KEY = OFF) ON [PRIMARY]
-) ON [PRIMARY]
-GO
-ALTER TABLE [Cadastro].[cliente]  WITH CHECK ADD  CONSTRAINT [FK_cliente_cliente] FOREIGN KEY([IdCliente])
-REFERENCES [Cadastro].[cliente] ([IdCliente])
-GO
-ALTER TABLE [Cadastro].[cliente] CHECK CONSTRAINT [FK_cliente_cliente]
-GO
-ALTER TABLE [Cadastro].[Viagem]  WITH CHECK ADD  CONSTRAINT [FK_tb_Viagem] FOREIGN KEY([IdCliente])
-REFERENCES [Cadastro].[cliente] ([IdCliente])
-GO
-ALTER TABLE [Cadastro].[Viagem] CHECK CONSTRAINT [FK_tb_Viagem]
-GO
-USE [master]
-GO
+
 ALTER DATABASE [BDAgenciaTurismo] SET  READ_WRITE 
 GO
-
--- BancodeDados/README.md
+++ BancodeDados/README.md
@@ -29,16 +29,6 @@ III. Modelo Físico do Banco de Dados:
 0. Utilizei o Microsoft SQL Server Management Studio, para realizar as atividades descritas a seguir;
 1. Criei o Banco de dados da Agencia de Turismo;
 2. Criei um esquema chamado Cadastro;
-3. Criei a tabela 'cliente', e em propriedades, selecionei o esquema 'Cadastro', criado anteriormente, depois, inclui na Tabela todos os Atributos que constam no Modelo Conceitual na Entidade cliente. Após isso, defini a PK e habilitei o Auto Increment, depois desmarquei todos os check list de 'Permitir Nulos', deixei apenas a coluna 'complemento', com a permissão de nulo. Em seguida comecei a redefinir os Tipos de dados usando o seguinte raciocinio:
-   -> idCliente - mantive o tipo: Int . Porque acredito que o número possivel de adeque melhor a este tipo.
-   -> CPF - alterei para Char(11), porque ele é ideal para valores fixos e obrigatórios, como: CEP, CPF, e etc, além disso esse tipo usa apenas 1 byte por caracter.
-   -> nomeCompleto - mantive o varchar(350), porque ele é ideal para valores variáveis, como: nome, rua, cidade e etc. Além disso, ele também ocupa apenas 1 byte por caracter.
-   -> logradouro - mantive varchar(250).
-   -> numero - usei o smallint, porque o numero de uma residencia não ultrapassa o valor 32767, além disso esse tipo não ocupa mais do que 2bytes.
-   -> complemento - usei o varchar(250).
-   -> cep - usei o tipo char(10).
-   -> bairro - usei o tipo varchar(50).
-   -> email - usei nvarchar(100), caso alguém tenha criado um email com caracteres especiais de outros idiomas, como da china, koreia, e etc, apesar desse tipo ucupar 2 bytes por caractere, acredito que ele seja o mais adequado para representar todos os caracteres de todas as línguas do mundo.
-   (...), analisei todos os tipos de dados dessa tabela, e alterei para os tipos que ocupam menos espaço e que de adequavam a cada tipo de coluna.
-4. Criei a tabela 'Viagem', fiz o mesmo procedimento descrito acima, e em seguida fiz o relacionamento entre a tabela 'Cliente' e a tabela 'Viagem'.
-5. Durante a construção do BD físico utilizei os seguintes recursos e comandos do SQL: DQL, DML e DDL.
+3. Criei as tabelas das entidades do BD Conceitual e Logico, defini as PK, e em propriedades, selecionei o esquema 'Cadastro', criado anteriormente, depois, inclui na Tabela todos os Atributos que constam no Modelo Conceitual e Lógico da respectiva Entidade. Após isso, defini a PK e habilitei o Auto Increment. Redefini também os Tipos de dados, visando ocupar o minimo de espaço.
+4. Após criar todas as Tabelas de todas as Entidades, defini as chaves estrangeiras. fiz o relacionamento entre as tabelas.
+5. Para contruir o BD físico utilizei os seguintes recursos e comandos do SQL: DQL, DML e DDL.
